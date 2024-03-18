f19e12753999cbab4a609213e58ef22d82402a47
boost1ne <boustane.chaimaa@gmail.com>
2024-03-18 15:27:56 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    count_arg = len(argv)
+    if (count_arg - 1) == 0:
+        print("0 arguments.")
+    elif (count_arg - 1) == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count_arg - 1))
+    for i in range(count_arg):
+        print("{}: {}".format(i, argv[i]))
