f26fab166a57377df812d4f8a4f990db16f0ec69
boost1ne <boustane.chaimaa@gmail.com>
2024-03-24 14:44:34 -0700

    fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-
-    for num in set(my_list):
-        sum += n
-
-    return sum
+        sum = 0
+            for num in set(my_list):
+                        sum += num
+                            return sum
