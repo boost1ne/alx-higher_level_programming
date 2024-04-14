f7b2e4f5a84a04327409aae09ebbed36b74440c5
boost1ne <boustane.chaimaa@gmail.com>
2024-04-14 09:14:19 -0700

    safe int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    iter = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            iter += 1
-        except (TypeError, ValueError):
-            continue
-    print()
-    return iter
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        else- True.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
