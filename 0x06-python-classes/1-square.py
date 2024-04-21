eb49e2c9f67c3af8d125892d8c40d8707b2d08a8
boost1ne <boustane.chaimaa@gmail.com>
2024-04-21 04:16:23 -0700

    private field

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,8 @@
-#!/usr/bin/pyhthon3
-
-"""Define class square"""
+#!/usr/bin/python3
+""" define a class Square"""
 
 
 class Square():
-
-    """the class square."""
-
+    """ the class square """
     def __init__(self, __size) -> None:
-        self.__size == __size
+        self.__size = __size
