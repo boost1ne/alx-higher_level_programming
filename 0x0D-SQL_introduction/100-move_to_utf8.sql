21e4b4d8dff8282147bafc57760a3969201f615c
boost1ne <boustane.chaimaa@gmail.com>
2024-05-23 08:55:50 -0700

    o

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Convert db characterset to utf-8 unicode
 
+USE hbtn_0c_0;
 ALTER hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
