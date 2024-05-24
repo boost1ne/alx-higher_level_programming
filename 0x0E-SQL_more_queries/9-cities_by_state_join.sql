88cf916dc00cb279416862edeeaf8068454ebe20
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 14:14:25 -0700

    ffs

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 --Display attributes from different tables
 
-SELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states
+ON cities.state_id=states.id
 ORDER BY cities.id ASC;
