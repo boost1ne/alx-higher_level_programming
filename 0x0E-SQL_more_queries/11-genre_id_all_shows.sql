af634a3888abc8481217e655be8a7a446d422bd0
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 15:14:21 -0700

    ffs

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,7 @@
---DOC DOC
+--Display tv all show titles and some genres id from 2 tables.
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_showd.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres
+ON tv_showd.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
