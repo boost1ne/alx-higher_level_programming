4c7117a4b1504efd99d22b6f2c5ce6dbcf28c484
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 15:42:17 -0700

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,7 @@
---List of all genres of the show Dexter.
+-- List of all genres of the show Dexter.
+
 SELECT name FROM tv_genres
-JOIN tv_show_genres ON id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name;
