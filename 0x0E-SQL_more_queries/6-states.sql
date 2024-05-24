193f03efc3485d343cc52e68459fd128b7f47827
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 11:28:03 -0700

    pk

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Create a table with an auto-generated uniq pk id att.
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 );
