f4245d185e020f4ca964a05af561680c546dca86
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 13:26:49 -0700

    subQ

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
--- cities from California state
+-- List cities id and names from California state using a subquery
 
-USE hbtn_0d_usa;
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = "California") 
+ORDER BY cities.id ASC;
 
