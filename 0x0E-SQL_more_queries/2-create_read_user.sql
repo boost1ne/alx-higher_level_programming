c31ad9d7e2a85bf7f8f2a6309b8e1201fbe4a96b
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 10:23:31 -0700

    grant read only

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create a new db with a user who has only read access privilege.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
