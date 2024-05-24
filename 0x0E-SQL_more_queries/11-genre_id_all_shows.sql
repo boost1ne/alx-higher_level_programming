ce087b21b8d6a0c09b475eec40a20edf61972ae1
boost1ne <boustane.chaimaa@gmail.com>
2024-05-24 15:06:07 -0700

    all from left

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,6 @@
---DOC DOC--
+--DOC DOC
 
-SELECT tv_shows.title, tv_show_genres.genre_id 
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_showd.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
