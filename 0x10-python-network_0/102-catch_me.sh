8a2f4e0eaa0b340f4b5172f094589a75fc49b269
boost1ne <boustane.chaimaa@gmail.com>
2024-07-21 14:14:13 -0700

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me causing the server to respond with a msg containing 'You got me!' in res body
+# Makes a request to 0.0.0.0:5000/catch_me causing the server to respond
+#with a msg containing 'You got me!' in res body
 curl -s 0.0.0.0:5000/catch_me
