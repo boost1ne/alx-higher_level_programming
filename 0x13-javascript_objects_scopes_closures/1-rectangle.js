395b9bd855d6be191c226f130a543c5f63f4e257
boost1e <boustane.chaimaa@gmail.com>
2024-07-29 12:00:22 -0700

    export class from module and make it available outside

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* A class Rectangle that defines a rectangle, initializing instance attributes width and height */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
